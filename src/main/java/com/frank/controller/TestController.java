62efb63b81d4fe6b0f6a2d710ec45a85948f27ec
Administrator <Administrator@192.168.0.108>
2018-04-14 18:58:46 +0800

    f

--- src/main/java/com/frank/controller/TestController.java
+++ src/main/java/com/frank/controller/TestController.java
@@ -1,17 +1,15 @@
 package com.frank.controller;
 
 
-import com.frank.bean.User;
-import com.frank.mapper.UserMapper;
-import com.frank.service.UserService;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.frank.mapper.UserMapper;
 
 @Controller
 @RequestMapping("test")
@@ -20,9 +18,7 @@ public class TestController {
     @Autowired
     private UserMapper mapper;
     
-    @Autowired
-    private UserService userService;
-
+   
 
     @RequestMapping("/firstTest")
     @ResponseBody
@@ -39,17 +35,17 @@ public class TestController {
         return map;
     }
     
-    @RequestMapping("/testTransaction")
-    @ResponseBody
-    public Map testTransaction() throws Exception{
-
-
-    	userService.testUnQueryCommit();
-        Map map = new HashMap();
-        map.put("s","s");
-        
-        return map;
-    }
+//    @RequestMapping("/testTransaction")
+//    @ResponseBody
+//    public Map testTransaction() throws Exception{
+//
+//
+//    	userService.testUnQueryCommit();
+//        Map map = new HashMap();
+//        map.put("s","s");
+//        
+//        return map;
+//    }
     
     
 
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,7 +1,19 @@
+import java.lang.reflect.Proxy;
+
+import com.frank.invocationHandler.LogInvocatoinHandler;
+import com.frank.mapper.Car;
+import com.frank.service.CarImpl;
+
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("¹þ¹þ");
+       
+    	Car car = new CarImpl();
+    	Car carProxy = (Car)Proxy.newProxyInstance(car.getClass().getClassLoader(),
+    			car.getClass().getInterfaces(), new LogInvocatoinHandler(car));
+    	carProxy.move(10000L);
+    	
+    	
     }
 
 }
